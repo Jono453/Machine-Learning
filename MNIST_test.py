a6f8ddd345a8f786da64a4a6bc2042a00ec51276
Jono453 <32658975+Jono453@users.noreply.github.com>
2018-07-29 13:36:43 +1000

    updated based on MNIST tutorial

--- MNIST_test.py
+++ MNIST_test.py
@@ -1,42 +1,52 @@
+#MNIST tutorial + integration onto Raspberry Pi
+
 import tensorflow as tf
 #print(tf.__version__)
 
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Convolution2D, MaxPooling2D
-from keras.utils import np_utils
-from keras.datasets import mnist
- 
-# Load pre-shuffled MNIST data into train and test sets
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-#print (X_train.shape)
-from matplotlib import pyplot as plt
-#plt.imshow(X_train[0])
+from tf.example.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-# 5. Preprocess input data
-X_train /= 255
-X_test /= 255
+#data set contains 55000 data for training. 10000 for test and 5000 for validation
+#every data point has two parts. image of handwritten digit (x) + label (y)
 
-# Model architecture
-model = Sequential()
+#28x28 image
+#mnist.train.images flattened. pixel array of 28*28 = 784
+#every image is a digit between 0 and 9. 
 
-#---Set up Layers---
+x = tf.placeholder(tf.float32,[None, 784]) #value to input any number of MNIST images
 
+#Weights and Bias]
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
 
-# Compile model
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+#implement model
+y = tf.nn.softmax(tf.matmul(x,W)+b)
+
+#training
+y_ = tf.placeholder(tf.float32,[None,10])
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+#reduce_mean calculates the mean over all examples in batch
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+sess = tf.InteracticeSession()
+tf.global_variables_initializer().run()
+
+#rrun training steps
+#at each step in for loop. get batch of one hundred random points from train set. 
+for _ in range(1000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, fee_dict={x: batch_xs, y_: batch_ys})
+  
+#Evaluating model
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = f.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
 '''
-# 9. Fit model on training data
-model.fit(X_train, Y_train, 
-          batch_size=32, nb_epoch=10, verbose=1)
- 
-# 10. Evaluate model on test data
-score = model.evaluate(X_test, Y_test, verbose=0)
-'''
-#Saving trained Keras CNN model
-#from keras.models import load_model
-#model.save('mnist_trained_model.h5')  # creates a HDF5 file
+#Saving trained Keras CNN model for use on hardware
+from keras.models import load_model
+model.save('mnist_trained_model.h5')  # creates a HDF5 file
 #on Pi -> model = load_model('my_model.h5')
+'''

a799f5baa891093a8f98131b5c7f1a7bf6cd3cb0
Jono453 <32658975+Jono453@users.noreply.github.com>
2018-08-08 23:19:53 +1000

    added in start of i2c

--- pantilt.py
+++ pantilt.py
@@ -1,28 +1,30 @@
 #PanTilt code
 
-'''
-Pseudocode:
-1. Detect red object
-2. Remove noise
-3. Create bounding box
-4. PD controller for x and y motion of mechanism
-5. Calculate offset in x and y directions.
-6. Rotate motors until object is centre of camera field of view
-(Later - machine learning/classifier to add on)
-'''
-
 import cv2
 import numpy as np
 from picamera.array import PiRGBArray
-from picamera import PiCamera
-from gpiozero import Motor
+import smbus
+
+import smbus
+import time
+bus = smbus.SMBus(1)
+
+address = 0x04 #for 12C with Arduino
+
+def writeNumber(value):
+    bus.write_byte(address,value)
+    return -1
+
+def readNumber():
+    number = bus.read_byte(address)
+    return number
 
 imageWidth  = 320                       # Camera image width
 imageHeight = 240                       # Camera image height
 
 camera = PiCamera()
 camera.resolution = (imageWidth,imageHeight)
-camera.framerate = 15
+camera.framerate = 18
 frameCapture = PiRGBArray(camera,size = (imageWidth,imageHeight))
 
 for frame in camera.capture_continuous(frameCapture,format = "bgr", use_video_port=True):
@@ -45,42 +47,56 @@ for frame in camera.capture_continuous(frameCapture,format = "bgr", use_video_po
     
     mask = cv2.inRange(hsv,redLower,redUpper)
     #result = cv2.bitwise_and(image,image,mask = mask)
+    cv2.imshow("test",mask)
     
     #Using contours for error calculation
     #each contour is a numpy array of (x,y) coordinates = boundary of a shape
-    img, contours, hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    img,contours,hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
     #Create bounding rectangle
     minArea = 200 #threshold (can modify later to get largest)
     c_max = 1
     #Setpoint
-    sp_x = imageWidth/2
-    sp_y = imageHeight/2
+    sp_x = imageWidth/2 #160
+    sp_y = imageHeight/2 #120
     
     for c in contours:
         CArea = cv2.contourArea(c)
         if CArea < minArea:
             continue
         x,y,w,h = cv2.boundingRect(c)
-        cv2.drawMarker(img,(sp_x,sp_y),(255,0,255),cv2.MARKER_CROSS,5,1) 
+        #cv2.drawMarker(img,(sp_x,sp_y),(255,0,255),cv2.MARKER_CROSS,5,1) 
 
         img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
         cv2.imshow("test",img)
 
         #Measured Centre of Box
         measured_x = x + w/2
-        measured_y = y - h/2
+        measured_y = y + h/2
 
+        #print("image shape",img.shape)
+        #print("Image_X: ", sp_x)
+        #print("Image_Y: ", sp_y)
+        #print("BoxCentre_X: ", measured_x)
+        #print("BoxCentre_Y: ", measured_y)
+         
         #Error
         error_x = abs(sp_x - measured_x)
         error_y = abs(sp_y - measured_y)
 
-        print("Error [x]: %d" %error_x)
-        print("Error [y]: %d\n" %error_y)
+        print("Error_x: ", error_x)
+        print("Error_y: ", error_y)
+        print("\n")
     
-        #move motors
-        #PanMotor = Motor(4,14)
-        #TiltMotor = Motor(5,10)
+        #Command servo motors to turn on Arduino
+        while ():
+            var = input("Test")
+
+            writeNumber(var)
+            print("Number sent was: ", var)
+            time.sleep(1)
+            number = readNumber()
+            print("I received: ", number)
 
     #GUI stuff
     key = cv2.waitKey(5) & 0xFF

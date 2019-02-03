9c64ff5f38554b2d693e4240b8dbf53275f5be8a
Jono453 <32658975+Jono453@users.noreply.github.com>
2019-02-03 22:21:22 +1100

    Delete regression_pract.py

--- regression_pract.py
+++
@@ -1,4 +0,0 @@
-#TF - Regression tutorial. 
-#regression problem, we aim to predict the output of a continuous value, like a price or a probability.
-#Contrast this with a classification problem, where we aim to predict a discrete label (for example,
-#where a picture contains an apple or an orange).
